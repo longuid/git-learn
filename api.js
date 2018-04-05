a8420654f6d32a1d3dca3e1a56f6701d23bebf6f
Yinxiong <zhangyinxiong@meituan.com>
2018-04-05 15:08:35 +0800

    update router/api

--- api.js
+++ api.js
@@ -12,10 +12,7 @@ export default {
         })
     },
 
-    update (id, data) {
-        return fetch(url, {
-            method: 'post',
-            data: JSON.stringify(data)
-        })
+    fetch () {
+        return fetch(url)
     }
 }
