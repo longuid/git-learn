4accce09f0fb0b95f74342bb2bf7f731cc6aa3b3
Yinxiong <zhangyinxiong@meituan.com>
2018-04-05 11:13:22 +0800

    add router api file

---
+++ api.js
@@ -0,0 +1,14 @@
+const url = '/api'
+
+export default {
+    get (id) {
+        return fetch(url)
+    },
+
+    post (id, data) {
+        return fetch(url, {
+            method: 'post',
+            data: JSON.stringify(data)
+        })
+    }
+}
---
+++ router.js
@@ -0,0 +1,13 @@
+class Router {
+    constructor (options) {
+
+    }
+}
+
+export default new Router([{
+    path: '/'
+}, {
+    path: '/detail'
+}, {
+    path: '/edit'
+}])
